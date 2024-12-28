04b7a9438e05de8f62568df19cda09e5354b3545
AnukaFonseka <akunafonseka@gmail.com>
2024-12-28 06:29:57 +0530

    Refactor API base URL to use production server

--- src/store/api/authApi.js
+++ src/store/api/authApi.js
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const authApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/" }),
-  // baseQuery: fetchBaseQuery({
-  //   baseUrl:
-  //     "https://product-management-dashboard-be-production.up.railway.app/",
-  // }),
+  // baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl:
+      "https://product-management-dashboard-be-production.up.railway.app/",
+  }),
 
   reducerPath: "authApi",
   endpoints: (build) => ({
